e84b86a41c84b0a46255b980ba0fccc87aaf0b26
Yusuf SEVİLGEN <Yusufsevilgenonline@gmail.com>
2021-12-07 12:06:11 +0300

    Skills section add skill

---
+++ src/components/data/skills.js
@@ -0,0 +1,31 @@
+export const SkillsData =
+  [
+    {
+      type: "Frontend",
+      list: [
+        {
+          name: "ReactJS",
+          icon: (
+            <i class="devicon-react-original colored"></i>
+          ),
+        },
+        {
+          name: "Javascript",
+          icon: (
+            <i class="devicon-javascript-plain colored"></i>
+          ),
+        },
+      ],
+    },
+    {
+      type: "Backend",
+      list: [
+        {
+          name: "Nodejs",
+          icon: (
+            <i class="devicon-nodejs-plain colored"></i>
+          ),
+        },
+      ],
+    },
+  ];
--- src/components/data/social.js
+++ src/components/data/social.js
@@ -1,32 +1,45 @@
-export const SocialData = [
+export const SocialData =
+  [
     {
-        platform: "Instagram",
-        link: "www.instagram.com",
-        icon: require("../../assets/icons/instagram.png").default,
+      platform:
+        "Instagram",
+      link: "www.instagram.com",
+      icon: require("../../assets/icons/instagram.png")
+        .default,
     },
     {
-        platform: "Github",
-        link: "www.github.com",
-        icon: require("../../assets/icons/github.png").default,
+      platform:
+        "Facebook",
+      link: "www.linkedin.com",
+      icon: require("../../assets/icons/facebook.png")
+        .default,
     },
     {
-        platform: "Linkedin",
-        link: "www.linkedin.com",
-        icon: require("../../assets/icons/linked.png").default,
+      platform:
+        "Github",
+      link: "www.github.com",
+      icon: require("../../assets/icons/github.png")
+        .default,
     },
     {
-        platform: "Facebook",
-        link: "www.linkedin.com",
-        icon: require("../../assets/icons/facebook.png").default,
+      platform:
+        "Linkedin",
+      link: "www.linkedin.com",
+      icon: require("../../assets/icons/linked.png")
+        .default,
     },
     {
-        platform: "Youtube",
-        link: "www.linkedin.com",
-        icon: require("../../assets/icons/youtube.png").default,
-    },    {
-        platform: "Twitter",
-        link: "www.linkedin.com",
-        icon: require("../../assets/icons/twitter.png").default,
-    }
-
-];
+      platform:
+        "Youtube",
+      link: "www.linkedin.com",
+      icon: require("../../assets/icons/youtube.png")
+        .default,
+    },
+    {
+      platform:
+        "Twitter",
+      link: "www.linkedin.com",
+      icon: require("../../assets/icons/twitter.png")
+        .default,
+    },
+  ];
